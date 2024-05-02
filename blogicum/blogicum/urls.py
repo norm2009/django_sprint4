39f59e962ab4a5cd0f058af850a9efef494cc842
Nikita <norm2009@ya.ru>
2024-05-02 21:51:45 +0300

    commit 4_tests

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -26,11 +26,12 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('django.contrib.auth.urls')),
-    path('auth/registration/',CreateView.as_view(
-        template_name='registration/registration_form.html',
-        form_class=BaseUserCreationForm,
-        success_url=reverse_lazy('pages:about'),
-    ),
+    path('auth/registration/',
+         CreateView.as_view(
+             template_name='registration/registration_form.html',
+             form_class=BaseUserCreationForm,
+             success_url=reverse_lazy('pages:about'),
+         ),
          name='registration'),
     path('pages/', include('pages.urls', namespace='pages')),
     path('', include('blog.urls', namespace='blog')),
