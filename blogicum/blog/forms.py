93b2a59385e7934cf9e452b6a80fd8efb5053c46
norm2009 <47937936+norm2009@users.noreply.github.com>
2024-05-03 19:46:24 +0300

    Update forms.py

--- blogicum/blog/forms.py
+++ blogicum/blog/forms.py
@@ -9,16 +9,16 @@ User = get_user_model()
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'username', 'email']
+        fields = ('first_name', 'last_name', 'username', 'email')
 
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'text', 'location', 'category', 'pub_date', 'image']
+        fields = ('title', 'text', 'location', 'category', 'pub_date', 'image')
 
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ['text']
+        fields = ('text')
