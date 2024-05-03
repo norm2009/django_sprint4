3a1dfb88175073eee40cabf45447d47eb90c3be3
norm2009 <47937936+norm2009@users.noreply.github.com>
2024-05-03 18:22:47 +0300

    Update views.py

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -7,3 +7,15 @@ class AboutTemplateView(TemplateView):
 
 class RulesTemplateView(TemplateView):
     template_name = 'pages/rules.html'
+
+
+def csrf_failure(request, reason=''):
+    return render(request, 'pages/403csrf.html', status=403)
+
+
+def page_not_found(request, exception):
+    return render(request, 'pages/404.html', status=404)
+
+
+def e_handler500(request, *args, **kwargs):
+    return render(request, 'pages/500.html', status=500)
